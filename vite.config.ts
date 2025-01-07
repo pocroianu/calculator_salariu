4733eb903e7798b02e4957ab6711ceb82d31d3c8
Geani Pocroianu <nicolaepocroianu@waterford.org>
2025-01-08 00:13:51 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/calculator_salariu/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
